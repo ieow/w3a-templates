d5145b0e7be400176b5936733ebc748deefcc404
aidanaden <aidan@u.nus.edu>
2024-08-21 18:58:36 +0800

    test: remove polyfil;;

--- w3a-mpc-start/app.config.ts
+++ w3a-mpc-start/app.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "@solidjs/start/config";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
   ssr: false,
@@ -24,18 +23,18 @@ export default defineConfig({
       global: "globalThis",
     },
     plugins: [
-      nodePolyfills({
-        //   // To add only specific polyfills, add them here. If no option is passed, adds all polyfills
-        include: ["buffer"],
-        //   // To exclude specific polyfills, add them to this list. Note: if include is provided, this has no effect
-        //   // exclude: [
-        //   //   "http", // Excludes the polyfill for `http` and `node:http`.
-        //   // ],
-        // Whether to polyfill specific globals.
-        globals: {
-          Buffer: true, // can also be 'build', 'dev', or false
-        },
-      }),
+      // nodePolyfills({
+      //   //   // To add only specific polyfills, add them here. If no option is passed, adds all polyfills
+      //   include: ["buffer"],
+      //   //   // To exclude specific polyfills, add them to this list. Note: if include is provided, this has no effect
+      //   //   // exclude: [
+      //   //   //   "http", // Excludes the polyfill for `http` and `node:http`.
+      //   //   // ],
+      //   // Whether to polyfill specific globals.
+      //   globals: {
+      //     Buffer: true, // can also be 'build', 'dev', or false
+      //   },
+      // }),
     ],
   },
 });
