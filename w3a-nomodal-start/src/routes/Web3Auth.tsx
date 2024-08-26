f06f80154d90b5b58f7a88538b80be46d322d1e1
aidanaden <aidan@u.nus.edu>
2024-08-26 20:24:30 +0800

    test 2

--- w3a-nomodal-start/src/routes/Web3Auth.tsx
+++ w3a-nomodal-start/src/routes/Web3Auth.tsx
@@ -159,6 +159,14 @@ export const W3Auth: VoidComponent = () => {
     }
     const web3authProvider = await auth.connectTo(WALLET_ADAPTERS.OPENLOGIN, {
       loginProvider: "google",
+      extraLoginOptions: {
+        domain: auth0domainUrl,
+        // this corresponds to the field inside jwt which must be used to uniquely
+        // identify the user. This is mapped b/w google and github logins
+        verifierIdField: "email",
+        isVerifierIdCaseSensitive: false,
+        connection: "google-oauth2",
+      },
     });
     console.log({ web3authProvider });
     setProvider(web3authProvider ?? undefined);
