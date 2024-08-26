e21dd6913f65864a8ddfb76174b67daaebee1ca3
aidanaden <aidan@u.nus.edu>
2024-08-26 18:57:43 +0800

    test: devenet

--- w3a-nomodal-start/src/routes/Web3Auth.tsx
+++ w3a-nomodal-start/src/routes/Web3Auth.tsx
@@ -97,7 +97,7 @@ export const W3Auth: VoidComponent = () => {
       const web3auth = new Web3AuthNoModal({
         clientId: web3AuthClientId,
         privateKeyProvider,
-        web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_MAINNET,
+        web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
       });
 
       setWeb3auth(web3auth);
